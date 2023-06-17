7273b63e01a43aaa32c0dcf045088b8f071a6c8d
김상진, Kim Sang Jin <62230118+sangjinKIM93@users.noreply.github.com>
2023-06-17 15:16:01 +0900

    Initial Commit

---
+++ PlanDoSee/PlanDoSeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanDoSeeApp.swift
+//  PlanDoSee
+//
+//  Created by 김상진 on 2023/06/17.
+//
+
+import SwiftUI
+
+@main
+struct PlanDoSeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
